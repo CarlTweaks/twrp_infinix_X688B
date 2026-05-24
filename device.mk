db9d53883380bd29cabc6dbe1882469ac8f943d7
CarlTweaks <carltible0@gmail.com>
2026-05-25 04:33:04 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Fastbootd
 PRODUCT_PACKAGES += \
